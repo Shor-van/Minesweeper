20ea35b222626ab361360cd018645eea211207f8
Shor-van <shorxvan01@gmail.com>
2015-05-31 01:14:02 +0200

    More work on the intro screen

--- Minesweaper/Screens/IntroScreen.cs
+++ Minesweaper/Screens/IntroScreen.cs
@@ -9,28 +9,36 @@ namespace Minesweeper.Screens
 {
     public class IntroScreen
     {
-        private TitleText shadow; //The SHADOW TitleText
-        private TitleText games; //The GAMES TitleText
-
+        private TitleText shadow; //The SHADOW GAMES TitleText
+        private TitleText shorvan; //The Shor_van TitleText
+        
+        /// <summary>Initsalize the intro</summary>
         public IntroScreen()
         {
             shadow = new TitleText("SHADOW GAMES", ConsoleColor.Black, 0, 0);
-            games = new TitleText("GAMES", ConsoleColor.White, 0, 0);
+            shorvan = new TitleText("SHOR VAN", ConsoleColor.White, 0, 0);
 
             RecalcualtePositions();
         }
 
+        /// <summary>Recalculates all the positions</summary>
         private void RecalcualtePositions()
         {
             shadow.PositionX = (Program.ViewWidth() / 2) - (shadow.MeasureSize()[0] / 2);
             shadow.PositionY = 2;
+
+            shorvan.PositionX = (Program.ViewWidth() / 2) - (shorvan.MeasureSize()[0] / 2);
+            shorvan.PositionY = (Program.ViewHieght() - shorvan.MeasureSize()[1]) - 3;
         }
 
+        /// <summary>Draws objects that should only be drawn when the games switchs to this screen</summary>
         public void OneTimeDraw()
         {
             shadow.Draw();
+            shorvan.Draw();
         }
 
+        /// <summary>Updates the screen will mostly be the animation</summary>
         public void Update()
         {
             if (Program.switchingScreen)
@@ -45,6 +53,7 @@ namespace Minesweeper.Screens
             Program.switchingScreen = false;
         }
 
+        /// <summary>Draws the screen, is called every loop</summary>
         public void Draw()
         {
 
